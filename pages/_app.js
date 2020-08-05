b1f68b9f8199610f6ca3c44dfd78ab1c6339b9ca
Alexander Ignacz <alex.ignacz412@gmail.com>
2020-08-06 05:04:59 +0800

    Footer implementation

---
+++ pages/_app.js
@@ -0,0 +1,92 @@
+// import App from 'next/app'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <Component {...pageProps} />
+      <style jsx global>{`
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-Medium.ttf') format('truetype');
+                      font-weight: 500;
+                      font-style: normal;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-SemiBold.ttf') format('truetype');
+                      font-weight: 600;
+                      font-style: normal;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-MediumItalic.ttf') format('truetype');
+                      font-weight: 500;
+                      font-style: italic;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-BoldItalic.ttf') format('truetype');
+                      font-weight: bold;
+                      font-style: italic;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-Regular.ttf') format('truetype');
+                      font-weight: normal;
+                      font-style: normal;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-SemiBoldItalic.ttf') format('truetype');
+                      font-weight: 600;
+                      font-style: italic;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-Bold.ttf') format('truetype');
+                      font-weight: bold;
+                      font-style: normal;
+                      font-display: swap;
+                  }
+                  
+                  @font-face {
+                      font-family: 'Cabin';
+                      src: url('/fonts/Cabin-Italic.ttf') format('truetype');
+                      font-weight: normal;
+                      font-style: italic;
+                      font-display: swap;
+                  }
+                  
+                  body {
+                    font-family: 'Cabin';
+                  }
+                `}</style>
+    </div>
+  )
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -11,60 +11,18 @@ export default function Home() {
       <main>
         <div>
           <div>
-            <img src="/phone.png" />
+            <img src="/phone.png" className="img_phone"/>
           </div>
           <div>
 
           </div>
         </div>
-        {/*<h1 className="title">*/}
-        {/*  Welcome to <a href="https://nextjs.org">Next.js!</a>*/}
-        {/*</h1>*/}
-
-        {/*<p className="description">*/}
-        {/*  Get started by editing <code>pages/index.js</code>*/}
-        {/*</p>*/}
-
-        {/*<div className="grid">*/}
-        {/*  <a href="https://nextjs.org/docs" className="card">*/}
-        {/*    <h3>Documentation &rarr;</h3>*/}
-        {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a href="https://nextjs.org/learn" className="card">*/}
-        {/*    <h3>Learn &rarr;</h3>*/}
-        {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://github.com/zeit/next.js/tree/master/examples"*/}
-        {/*    className="card"*/}
-        {/*  >*/}
-        {/*    <h3>Examples &rarr;</h3>*/}
-        {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*    className="card"*/}
-        {/*  >*/}
-        {/*    <h3>Deploy &rarr;</h3>*/}
-        {/*    <p>*/}
-        {/*      Instantly deploy your Next.js site to a public URL with Vercel.*/}
-        {/*    </p>*/}
-        {/*  </a>*/}
-        {/*</div>*/}
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
+        <a className="link" href="/faq_v2.pdf" target="_blank">FAQ</a>
+        <a className="link" href="/ToS.pdf" target="_blank">Terms of Service</a>
+        <a className="link" href="/CC_Privacy_Policy.pdf" target="_blank">Privacy Policy</a>
       </footer>
 
       <style jsx>{`
@@ -90,9 +48,9 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
+          max-width: 900px;
           display: flex;
-          justify-content: center;
+          justify-content: space-around;
           align-items: center;
         }
 
@@ -111,26 +69,8 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
+        .img_phone {
+          width: 252px;
         }
 
         .description {
@@ -157,38 +97,9 @@ export default function Home() {
           margin-top: 3rem;
         }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
+        .link {
+          color: #929292;
+          font-size: 14px;
         }
 
         @media (max-width: 600px) {
